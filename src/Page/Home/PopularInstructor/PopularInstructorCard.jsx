deef7e1352061d3caee41300040ede70bbff0300
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-06-12 23:41:53 +0600

    popular instructor card modifed

--- src/Page/Home/PopularInstructor/PopularInstructorCard.jsx
+++ src/Page/Home/PopularInstructor/PopularInstructorCard.jsx
@@ -10,7 +10,7 @@ const PopularInstructorCard = ({ instructor }) => {
             .then(data => {
                 const classesFilter = data.filter(df => df.status === 'approve')
                 console.log(classesFilter)
-                const totalStudent = classesFilter.reduce((sum , student)=> student.enrolled + sum, 0)
+                const totalStudent = classesFilter.reduce((sum, student) => student.enrolled + sum, 0)
                 // cart.reduce((sum, item) => item.price + sum, 0) 
                 setClasses(totalStudent)
                 // console.log(data)
@@ -18,36 +18,36 @@ const PopularInstructorCard = ({ instructor }) => {
     }, [instructor.email])
     return (
         <div>
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-        
-              >
+            <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+
+            >
 
 
 
                 <div className="card bg-base-100 group w-full rounded-sm shadow-xl">
 
-                <figure><img className="h-80 transition group-hover:scale-110 w-full" src={instructor?.image} alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title text-2xl">{instructor?.name}</h2>
-    
-    
-                    <p className="font-semibold text-xl">Email : {instructor?.email}</p>
-                    <p className="font-semibold text-xl">Total Student : {classes}</p>
-                   
-    
-    
-           
-    
+                    <figure><img className="h-80 transition group-hover:scale-110 w-full" src={instructor?.image} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-2xl">{instructor?.name}</h2>
+
+
+                        <p className="font-semibold text-xl">Email : {instructor?.email}</p>
+                        <p className="font-semibold text-xl">Total Enroll : {classes}</p>
+
+
+
+
+
+                    </div>
                 </div>
-            </div>
             </motion.div>
-        
-        
-        
+
+
+
         </div>
-      
+
     );
 };
 
